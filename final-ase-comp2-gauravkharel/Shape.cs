7404abd5184ace8a3847bb9da1e2769495b664fb
gauravkharel <kgaurav21@tbc.edu.np>
2021-05-13 08:22:51 +0545

    The file architecture is created and now, working on the application for Component1

---
+++ final-ase-comp2-gauravkharel/Shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace final_ase_comp2_gauravkharel
+{
+    interface Shape
+    {
+    }
+}
